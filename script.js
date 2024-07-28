cbdc8f224f98bc0623a180a3194e8c4bff3ef12c
Alex Sidell <51405895+alexsidell@users.noreply.github.com>
2024-07-28 23:42:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,5 @@ topMenuEl.classList.add("flex-around")
 
 //Task 3
 for (link in menuLinks) {
-    console.log(menuLinks[link])
     topMenuEl.innerHTML += `<a href ${menuLinks[link].href}>${menuLinks[link].text}`
 }
